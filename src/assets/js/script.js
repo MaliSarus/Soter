184d9220fcfac113f5b89ac99831bf99ec2a5052
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-04-15 17:45:11 +0300

    sponsors.php

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,30 +2,90 @@ function isSet(element) {
     return element.length !== 0;
 }
 
-$(document).ready(function () {
+const hamburgerInit = () => {
     const hamburger = $('.hamburger');
     hamburger.css({
-        outline:'none'
+        outline: 'none'
     });
-    $('.hamburger').on('click',function () {
+    $('.hamburger').on('click', function () {
         $(this).toggleClass('is-active');
     });
+};
 
-    if(isSet($('.sponsors'))){
-        var mySwiper = new Swiper ('.swiper-container', {
-            // Optional parameters
-            slidesPerView: 5,
-            spaceBetween: 115,
-            direction: 'horizontal',
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-                renderBullet: function (index, className) {
-                    return '<span class="' + className + '"></span>';
-                },
+const sliderButtonsHandler = () =>{
+    const btns = $('.nav-dot');
+    const paginationWrapper = $('.pagination-wrapper');
+    const bigDotContainer = $('.big-dot-container');
+    const littleDot = $('.little-dot');
+
+    btns.on('click', btnClick);
+
+    function btnClick() {
+        if($(this).hasClass('little-dot--first')) {
+            paginationWrapper.addClass('transition-prev');
+        } else if ($(this).hasClass('little-dot--last')){
+            paginationWrapper.addClass('transition-next');
+        }
+
+        var timeout = setTimeout(cleanClasses, 500);
+    }
+
+    function cleanClasses() {
+        if(paginationWrapper.hasClass('transition-next')) {
+            paginationWrapper.removeClass('transition-next')
+        } else if(paginationWrapper.hasClass('transition-prev')) {
+            paginationWrapper.removeClass('transition-prev')
+        }
+    }
+}
+
+const sponsorsSlideInit = () => {
+    const mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        slidesPerView: 1,
+        spaceBetween: 0,
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 2,
+                spaceBetween: 40,
             },
-        })
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+            992: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+            },
+            1200: {
+                slidesPerView: 5,
+                spaceBetween: 115,
+            }
+        }
+    });
+    const dots = $('.sponsors__content .little-dot');
+    dots.on('click',function (event) {
+        if($('.little-dot--first').is(event.target)){
+            mySwiper.slidePrev(400,true);
+        }
+        if($('.little-dot--last').is(event.target)){
+            mySwiper.slideNext(400,true);
+        }
+    })
+};
+
+$(document).ready(function () {
+    hamburgerInit();
+    sliderButtonsHandler();
+    if (isSet($('.sponsors'))) {
+        sponsorsSlideInit();
     }
 });
 
--- src/modules/sponsors.php
+++ src/modules/sponsors.php
@@ -3,21 +3,36 @@
         <div class="row">
             <div class="col-12">
                 <div class="sponsors__block">
-                    <h2 class="sponsors__title title title_dark">Your Title</h2>
+                    <div class="sponsors__head">
+                        <h2 class="sponsors__title title title_dark">Your Title</h2>
+                        <div class="arrows">
+                            <div class="arrow arrow-prev"></div>
+                            <div class="arrow arrow-next"></div>
+                        </div>
+                    </div>
                     <div class="sponsors__content">
                         <div class="swiper-container">
-                            <!-- Additional required wrapper -->
                             <div class="swiper-wrapper">
-                                <!-- Slides -->
-                                <div class="swiper-slide"><img src="./assets/img/sponsors/nhs-logo.png" alt="Лого спонсора"></div>
-                                <div class="swiper-slide"><img src="./assets/img/sponsors/innovate-uk-logo.png" alt="Лого спонсора"></div>
-                                <div class="swiper-slide"><img src="./assets/img/sponsors/curtin-logo.png" alt="Лого спонсора"></div>
-                                <div class="swiper-slide"><img src="./assets/img/sponsors/csiro-logo.png" alt="Лого спонсора"></div>
-                                <div class="swiper-slide"><img src="./assets/img/sponsors/mriwa-logo.png" alt="Лого спонсора"></div>
-
+                                <div class="swiper-slide sponsors__slide"><img src="./assets/img/sponsors/nhs-logo.png"
+                                                                               alt="Лого спонсора"></div>
+                                <div class="swiper-slide sponsors__slide"><img
+                                        src="./assets/img/sponsors/innovate-uk-logo.png" alt="Лого спонсора"></div>
+                                <div class="swiper-slide sponsors__slide"><img
+                                        src="./assets/img/sponsors/curtin-logo.png" alt="Лого спонсора"></div>
+                                <div class="swiper-slide sponsors__slide"><img
+                                        src="./assets/img/sponsors/csiro-logo.png" alt="Лого спонсора"></div>
+                                <div class="swiper-slide sponsors__slide"><img
+                                        src="./assets/img/sponsors/mriwa-logo.png" alt="Лого спонсора"></div>
                             </div>
-                            <!-- If we need pagination -->
-                            <div class="swiper-pagination"></div>
+                        </div>
+                        <div class="pagination-wrapper">
+                            <div class="pagination-container">
+                                <div class="little-dot nav-dot little-dot--first"></div>
+                                <div class="little-dot">
+                                    <div class="big-dot"></div>
+                                </div>
+                                <div class="little-dot nav-dot little-dot--last"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
